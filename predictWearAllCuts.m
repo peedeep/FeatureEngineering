cb33bb2d35a4e6e9f06f28b00beefe1c5749b4bd
Rick <56639624@qq.com>
2018-12-10 19:26:27 +0800

    update predictWearAllCuts

--- predictWearAllCuts.m
+++ predictWearAllCuts.m
@@ -1,45 +1,57 @@
 %function [] = predictWearCuts()
 
 %% Predict train Wear Cuts
-P_B = csvread('./data/train_result.csv', 1);
-plot(max(P_B, [], 2));
-train_cuts = caculateWearMaxCuts(P_B);
+pred_b_wear = csvread('./data/train_result.csv', 1);
+figure;
+subplot(2, 1, 1);
+pred_maximum_b_wear = max(pred_b_wear, [], 2);
+plot(pred_maximum_b_wear);
+pred_b_cuts = caculateWearMaxCuts(pred_maximum_b_wear);
 %save result.txt cuts var -ascii;
 fid=fopen('./data/train_result.txt','wt');
-fprintf(fid,'%d\n', train_cuts); 
+fprintf(fid,'%d\n', pred_b_cuts); 
 fclose(fid);
 
-%% Predict test Wear Cuts
-P = csvread('./data/test_result.csv', 1);
 hold on;
-plot(max(P, [], 2));
-pred_cuts = caculateWearMaxCuts(P);
+real_maximum_b_wear = B_Y;
+plot(real_maximum_b_wear);
+legend('predB', 'realB');
+real_b_cuts = caculateWearMaxCuts(real_maximum_b_wear);
+
+%% Predict test Wear Cuts
+pred_test_wear = csvread('./data/test_result.csv', 1);
+subplot(2, 1, 2);
+pred_maximum_test_wear = max(pred_test_wear, [], 2);
+%figure;
+plot(pred_maximum_test_wear);
+pred_test_cuts = caculateWearMaxCuts(pred_maximum_test_wear);
 %save result.txt cuts var -ascii;
 fid=fopen('./data/test_result.txt','wt');
-fprintf(fid,'%d\n', pred_cuts); 
+fprintf(fid,'%d\n', pred_test_cuts); 
 fclose(fid);
 
 %% Real Wear Cuts
-R = csvread('./data/test.csv');
+real_test_wear = csvread('./data/test.csv');
 hold on;
-plot(max(R, [], 2));
-legend('train', 'test', 'real');
-real_cuts = caculateWearMaxCuts(R);
+real_maximum_test_wear = max(real_test_wear, [], 2);
+plot(real_maximum_test_wear);
+legend('predTest', 'realTest');
+real_test_cuts = caculateWearMaxCuts(real_maximum_test_wear);
 %save result.txt cuts var -ascii;
 fid=fopen('./data/real.txt','wt');
-fprintf(fid,'%d\n', real_cuts); 
+fprintf(fid,'%d\n', real_test_cuts); 
 fclose(fid);
 
 %% caculate score
-punishment1 = caculatePunishment(train_cuts, real_cuts);
-punishment = caculatePunishment(pred_cuts, real_cuts);
+punishment_b = caculatePunishment(pred_b_cuts, real_b_cuts);
+punishment_test = caculatePunishment(pred_test_cuts, real_test_cuts);
 
 %% caculate accuracy
-m = size(P, 1);
+m = size(pred_maximum_test_wear, 1);
 
 for i = 1:flute_size
-    fprintf('\n flute%d Train set 均方误差（MSE）: %f', i, sum((P_B(:, i) - R(:, i)).^2) / m);
-    fprintf('\n flute%d Test set 均方误差（MSE）: %f', i, sum((P(:, i) - R(:, i)).^2) / m);
+    fprintf('\n flute%d train b set 均方误差（MSE）: %f', i, sum((pred_maximum_b_wear - real_maximum_b_wear).^2) / m);
+    fprintf('\n flute%d Test set 均方误差（MSE）: %f', i, sum((pred_maximum_test_wear - real_maximum_test_wear).^2) / m);
 end
 
 %end
